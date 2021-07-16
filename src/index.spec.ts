bf09af8839babecb6305b3c3a5a37079e1e223a5
Drew Lytle <aml3ed@virginia.edu>
2021-07-16 10:53:47 -0400

    fix(Test): Make tests take in correct set as data
    undefined

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,17 @@
 import { calcReimbursment } from ".";
-import { set1 } from "../data";
+import { set1, set2, set3, set4 } from "../data";
 
 describe("Main testing sets", () => {
   it("should calculate set1 correctly", () => {
     expect(calcReimbursment(set1)).toBe(165);
   });
   it("should calculate set2 correctly", () => {
-    expect(calcReimbursment(set1)).toBe(620);
+    expect(calcReimbursment(set2)).toBe(620);
   });
   it("should calculate set3 correctly", () => {
-    expect(calcReimbursment(set1)).toBe(475);
+    expect(calcReimbursment(set3)).toBe(475);
   });
   it("should calculate set4 correctly", () => {
-    expect(calcReimbursment(set1)).toBe(215);
+    expect(calcReimbursment(set4)).toBe(215);
   });
 });
