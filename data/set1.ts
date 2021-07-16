66ffa30b3a4e346ac49c6a8510deec2d4ba4b5e7
Drew Lytle <aml3ed@virginia.edu>
2021-07-16 10:32:03 -0400

    refactor(Data): Make sets .ts files instead of JSON for ease of us
    undefined

---
+++ data/set1.ts
@@ -0,0 +1,7 @@
+export const set1 = [
+  {
+    cityCost: false,
+    startDate: "9/1/15",
+    endDate: "9/3/15"
+  }
+];
---
+++ data/set4.ts
@@ -0,0 +1,22 @@
+export const set4 = [
+  {
+    cityCost: false,
+    startDate: "9/1/15",
+    endDate: "9/1/15"
+  },
+  {
+    cityCost: false,
+    startDate: "9/1/15",
+    endDate: "9/1/15"
+  },
+  {
+    cityCost: true,
+    startDate: "9/2/15",
+    endDate: "9/2/15"
+  },
+  {
+    cityCost: true,
+    startDate: "9/2/15",
+    endDate: "9/3/15"
+  }
+];
